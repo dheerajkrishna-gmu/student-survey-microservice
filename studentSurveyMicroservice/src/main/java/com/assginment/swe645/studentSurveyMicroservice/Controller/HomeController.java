0286b47d4bc087028b468e3aa7053dc1a8de7eb4
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-04-15 20:24:11 -0400

    Added Home Controller

---
+++ studentSurveyMicroservice/src/main/java/com/assginment/swe645/studentSurveyMicroservice/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.assginment.swe645.studentSurveyMicroservice.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("")
+	public String home() {
+		return "Welcome to Student Survey Microservice";
+	}
+}
